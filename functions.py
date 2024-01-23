d864d05b2b127d9892e7a7a53ce04e9b9a36a08d
Chandrashekhar1207 <Chandra1808.be23@chitkara.edu.in>
2024-01-23 10:49:41 +0530

    this is commit to upload

---
+++ functions.py
@@ -0,0 +1,8 @@
+def is_armstrong_number(num):
+    num_str = str(num)
+    num_digits = len(num_str)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
+    return num == armstrong_sum
+
+
+
